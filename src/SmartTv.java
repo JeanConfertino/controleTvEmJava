c9369305ed963c21e74bb427a6f3906b3caaeb56
Jean Confertino <jeanconfertino2016@gmail.com>
2024-05-15 14:16:07 -0300

    Ajustes

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class SmartTv {
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("Canal: " + canal);
+    }
+
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("Canal: " + canal);
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Volume da TV aumentado para: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Volume da TV diminuido para: " + volume);
+    }
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,5 +1,24 @@
+import java.sql.SQLOutput;
+
 public class Usuario {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+
+//        exemplos de entradas
+        SmartTv smartTv = new SmartTv();
+        smartTv.ligar();
+        smartTv.aumentarVolume();
+        smartTv.mudarCanal(27);
+
+        System.out.println("TV está Ligada: " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status - Ligada?" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status - Desligada?" + smartTv.ligada);
+
+
     }
 }
